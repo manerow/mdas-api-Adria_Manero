11add428453be5f962e860f5020643ae32417f05
Adrià Manero <adriamanero97@gmail.com>
2023-03-16 07:52:09 +0100

    feat(domain): create the aggregate and its valueobjects

---
+++ src/main/java/com/mdas/api/g6/user/domain/valueobject/FavouritePokemonId.java
@@ -0,0 +1,12 @@
+package com.mdas.api.g6.user.domain.valueobject;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class FavouritePokemonId {
+    private Integer id;
+}
