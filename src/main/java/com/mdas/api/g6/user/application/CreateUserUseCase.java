d3ae19aa7ac6b284c10cddbb5c476de5b1737f37
Adrià Manero <adriamanero97@gmail.com>
2023-03-18 11:36:14 +0100

    fix: no interfaces for the usecases on the application layer

--- src/main/java/com/mdas/api/g6/user/application/CreateUserUseCase.java
+++ src/main/java/com/mdas/api/g6/user/application/CreateUserUseCase.java
@@ -2,7 +2,21 @@ package com.mdas.api.g6.user.application;
 
 import com.mdas.api.g6.user.domain.User;
 import com.mdas.api.g6.user.domain.exception.UserAlreadyExistsException;
+import com.mdas.api.g6.user.domain.services.UserCreator;
+import com.mdas.api.g6.user.domain.valueobject.UserId;
+import com.mdas.api.g6.user.domain.valueobject.UserName;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface CreateUserUseCase {
-    User execute(String name) throws UserAlreadyExistsException;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class CreateUserUseCase {
+
+    private final UserCreator userCreator;
+
+    public User execute(String name) throws UserAlreadyExistsException {
+        return userCreator.create(new UserId(UUID.randomUUID()), new UserName(name));
+    }
 }
