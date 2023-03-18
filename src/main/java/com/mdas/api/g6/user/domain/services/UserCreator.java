91c8e852628c65a30a7be55b30f28a84cd7fdc8d
Adrià Manero <adriamanero97@gmail.com>
2023-03-18 11:35:29 +0100

    fix: userCreator in charge of creating User instance

--- src/main/java/com/mdas/api/g6/user/domain/services/UserCreator.java
+++ src/main/java/com/mdas/api/g6/user/domain/services/UserCreator.java
@@ -3,8 +3,14 @@ package com.mdas.api.g6.user.domain.services;
 import com.mdas.api.g6.user.domain.User;
 import com.mdas.api.g6.user.domain.exception.UserAlreadyExistsException;
 import com.mdas.api.g6.user.domain.repository.UserRepository;
+import com.mdas.api.g6.user.domain.valueobject.FavouritePokemons;
+import com.mdas.api.g6.user.domain.valueobject.UserId;
+import com.mdas.api.g6.user.domain.valueobject.UserName;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.UUID;
 
 @Component
 @AllArgsConstructor
@@ -12,14 +18,16 @@ public class UserCreator {
 
     private UserRepository userRepository;
 
-    public void create(User user) throws UserAlreadyExistsException {
-        guard(user.getName().getName());
+    public User create(UserId id, UserName userName) throws UserAlreadyExistsException {
+        guard(userName.getName());
+        User user = new User(id, userName, new FavouritePokemons(new HashSet<>()));
         userRepository.save(user);
+        return user;
     }
 
     private void guard(String userName) throws UserAlreadyExistsException {
         if (userRepository.existsByName(userName)) {
-            throw new UserAlreadyExistsException("The username '" + userName + "' already exists");
+            throw new UserAlreadyExistsException("User already exists with name: " + userName);
         }
     }
 }
