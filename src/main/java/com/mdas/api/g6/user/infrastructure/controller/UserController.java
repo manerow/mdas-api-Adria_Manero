e927ee1a10f0898c1608250543193d5df08da524
Adrià Manero <adriamanero97@gmail.com>
2023-03-18 13:22:14 +0100

    feat: type the expected create user controller request body

--- src/main/java/com/mdas/api/g6/user/infrastructure/controller/UserController.java
+++ src/main/java/com/mdas/api/g6/user/infrastructure/controller/UserController.java
@@ -4,7 +4,8 @@ import com.mdas.api.g6.shared.infrastructure.controller.ApiResponse;
 import com.mdas.api.g6.user.application.CreateUserUseCase;
 import com.mdas.api.g6.user.domain.User;
 import com.mdas.api.g6.user.domain.exception.UserAlreadyExistsException;
-import lombok.RequiredArgsConstructor;
+import com.mdas.api.g6.user.infrastructure.controller.dto.CreateUserRequest;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 @Slf4j
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserController {
     private CreateUserUseCase createUserUseCase;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<User>> createUser(@RequestBody String name) {
+    public ResponseEntity<ApiResponse<User>> create(@RequestBody CreateUserRequest createUserRequest) {
         User user;
         try {
-            user = createUserUseCase.execute(name);
+            user = createUserUseCase.execute(createUserRequest.getName());
         } catch (UserAlreadyExistsException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .body(new ApiResponse<>(HttpStatus.CONFLICT, e.getMessage(), null));
---
+++ src/main/java/com/mdas/api/g6/user/infrastructure/controller/dto/CreateUserRequest.java
@@ -0,0 +1,16 @@
+package com.mdas.api.g6.user.infrastructure.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CreateUserRequest {
+    @JsonProperty("name")
+    private String name;
+}
