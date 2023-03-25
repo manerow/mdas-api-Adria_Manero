b9e0675360cf650c9773484fe8da3f4bdfe5daf3
Adrià Manero <adriamanero97@gmail.com>
2023-03-25 20:16:28 +0100

    fix: pokemon not found exception description

--- src/main/java/com/mdas/api/g6/pokedex/pokemon/domain/exception/PokemonNotFoundException.java
+++ src/main/java/com/mdas/api/g6/pokedex/pokemon/domain/exception/PokemonNotFoundException.java
@@ -2,6 +2,6 @@ package com.mdas.api.g6.pokedex.pokemon.domain.exception;
 
 public class PokemonNotFoundException extends Exception {
     public PokemonNotFoundException() {
-        super("The pokemon with the provided name was not found.");
+        super("The pokemon with the provided id was not found.");
     }
 }
